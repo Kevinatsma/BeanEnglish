f1fb68e740a267b1263c89254a3ecb4f451b207a
Kevin Atsma <kevinatsma@gmail.com>
2018-11-20 16:22:59 -0500

    Set up Firebase hosting and Real-time Database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB9tmKvrblP_IzBIQLUZBItgn_XMEqQ9xw',
+    authDomain: 'bean-english.firebaseapp.com',
+    databaseURL: 'https://bean-english.firebaseio.com',
+    projectId: 'bean-english',
+    storageBucket: 'bean-english.appspot.com',
+    messagingSenderId: '819205487650'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB9tmKvrblP_IzBIQLUZBItgn_XMEqQ9xw',
+    authDomain: 'bean-english.firebaseapp.com',
+    databaseURL: 'https://bean-english.firebaseio.com',
+    projectId: 'bean-english',
+    storageBucket: 'bean-english.appspot.com',
+    messagingSenderId: '819205487650'
+  }
 };
 
 /*
